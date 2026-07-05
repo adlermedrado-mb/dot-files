e6176e03b94e43103187ad280334f2860d3d296d
Adler Medrado <adler@adlermedrado.com.br>
2026-07-05 14:19:25 -0300

    fix(nvim): migrate treesitter config to nvim 0.12
      - switch nvim-treesitter to the main branch API
      - install parsers through lazy build step
      - enable native treesitter highlighting per filetype
      - keep lazy-lock.json tracked for reproducible plugin versions

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,60 @@
+local parsers = {
+  "lua",
+  "python",
+  "javascript",
+  "typescript",
+  "go",
+  "gomod",
+  "gosum",
+  "c",
+  "bash",
+  "markdown",
+  "markdown_inline",
+  "json",
+  "yaml",
+  "vim",
+  "vimdoc",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      ensure_installed = {
-        "lua","python","javascript","typescript","go","gomod","gosum","c","bash","markdown","markdown_inline","json","yaml","vim","vimdoc"
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          node_decremental = "<bs>",
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    branch = "main",
+    lazy = false,
+    build = function()
+      require("nvim-treesitter").install(parsers):wait(300000)
+    end,
+    config = function()
+      local treesitter = require("nvim-treesitter")
+      local install_dir = vim.fn.stdpath("data") .. "/site"
+
+      treesitter.setup({ install_dir = install_dir })
+
+      local enabled = {}
+      for _, lang in ipairs(parsers) do
+        enabled[lang] = true
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("user_treesitter", { clear = true }),
+        callback = function(args)
+          local ft = vim.bo[args.buf].filetype
+          local lang = vim.treesitter.language.get_lang(ft)
+
+          if lang and enabled[lang] then
+            pcall(vim.treesitter.start, args.buf, lang)
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+
+      vim.keymap.set({ "n", "x" }, "<C-space>", function()
+        vim.treesitter.select("parent", vim.v.count1)
+      end, { desc = "Expand selection" })
+
+      vim.keymap.set("x", "<bs>", function()
+        vim.treesitter.select("child", vim.v.count1)
+      end, { desc = "Shrink selection" })
     end,
   },
 }
